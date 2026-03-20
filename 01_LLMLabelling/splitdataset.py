43d66d113223994382f979a3d1ae4f82e74c219e
Devan <devanasokan@gmail.com>
2026-03-20 21:42:55 +0800

    week7_checkpoint

---
+++ 01_LLMLabelling/splitdataset.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Users/User/Documents/devanasokan_fyp/storage/structured_lyrics.csv", encoding='utf-8-sig')
+
+# Spilt dataset into small chunks of 100 rows
